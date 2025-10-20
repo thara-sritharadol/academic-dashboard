a9663110ca2ac567367e330149cdf3911025d714
Thara Sritharadol <sunsummerone1@gmail.com>
2025-10-20 17:17:04 +0700

    register topic embedding and classified topic in admin site

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Paper, ExtractedSkill, SkillEmbedding
+from .models import Paper, ExtractedSkill, SkillEmbedding, TopicEmbedding, ClassifiedTopic
 
 admin.site.register(Paper)
 admin.site.register(ExtractedSkill)
 admin.site.register(SkillEmbedding)
+admin.site.register(TopicEmbedding)
+admin.site.register(ClassifiedTopic)
