7675f1051dc24997c1f585454d59114dc4e96ef1
thara <sunsummerone1@gmail.com>
2026-04-26 21:37:15 +0700

    admin : unable admin page

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Paper, Author
 
+"""
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('name', 'faculty', 'department', 'primary_cluster')
@@ -9,4 +10,5 @@ class AuthorAdmin(admin.ModelAdmin):
 @admin.register(Paper)
 class paperAdmin(admin.ModelAdmin):
     list_display = ('title', 'authors_text', 'year')
-    search_fields = ('authors_text', 'id', 'cluster_label', 'openalex_concepts')
+    search_fields = ('authors_text', 'id', 'cluster_label', 'openalex_concepts')'
+"""
